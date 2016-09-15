3f2fb144cb254451d43c1cc483edc34070165ea9
Mark Oates <euph_boy@hotmail.com>
2016-09-14 22:30:46 -0400

    Expand example program to output success or error

--- main.cpp
+++ main.cpp
@@ -3,11 +3,19 @@
 
 #include "file_downloader.h"
 
+#include <iostream>
+
 
 int main(int, char**)
 {
    std::string file_url = "https://www.allegro.cc/theme/default/image/logo.png";
    FileDownloader::FileHandle handle = FileDownloader::download_file(file_url, "logo.png");
+
+   if (handle.get_status() == FileDownloader::DOWNLOADED)
+      std::cout << "File Downloaded Successfully!" << std::endl;
+   else
+      std::cout << "Download Error: " << handle.get_error() << std::endl;
+
    return 0;
 }
 
